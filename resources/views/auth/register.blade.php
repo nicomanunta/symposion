263164f886f432eb906c7d0bf32a86f33cbc4c2f
nicomanunta <nic.manun@gmail.com>
2025-01-22 17:27:37 +0100

    changes file registered blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,71 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
         @csrf
 
+    
         <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="form-group mb-3">
+            <x-input-label for="name" :value="__('Nome')" />
+            <input class="form-control mt-1" type="text" name="name" id="name" placeholder="Nome" value="{{ old('name')}}" required>
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />    
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <!-- Surname -->
+        <div class="form-group mb-3">
+            <x-input-label for="surname" :value="__('Cognome')" />
+            <input class="form-control mt-1" type="text" name="surname" id="surname" placeholder="Cognome" value="{{ old('surname')}}" required>
+            <x-input-error :messages="$errors->get('surname')" class="mt-2" />    
         </div>
+
+        <!-- Email  -->
+        <div class="form-group mb-3">
+            <x-input-label for="email" :value="__('Email')" />
+            <input class="form-control mt-1" type="text" name="email" id="email" placeholder="Cognome" value="{{ old('email')}}" required>
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />    
+        </div>
+        
+        <!-- Phone -->
+        <div class="form-group mb-3">
+            <x-input-label for="phone" :value="__('Telefono')" />
+            <input class="form-control mt-1" type="text" name="phone" id="phone" placeholder="Cognome" value="{{ old('phone')}}" >
+            <x-input-error :messages="$errors->get('phone')" class="mt-2" />    
+        </div>
+
+        <!-- Region -->
+        <div class="form-group mb-3">
+            <x-input-label for="region" :value="__('Regione')" />
+            <select class="form-control mt-1" name="region" id="region">
+                <option value="Abruzzo {{old('region') == 'Uomo' ? 'selected' : ''}}">Abruzzo</option>
+                <option value="Basilicata">Basilicata</option>
+                <option value="Calabria">Calabria</option>
+                <option value="Campania">Campania</option>
+                <option value="Emilia-Romagna">Emilia-Romagna</option>
+                <option value="Friuli-Venezia Giulia">Friuli-Venezia Giulia</option>
+                <option value="Lazio">Lazio</option>
+                <option value="Liguria">Liguria</option>
+                <option value="Lombardia">Lombardia</option>
+                <option value="Marche">Marche</option>
+                <option value="Molise">Molise</option>
+                <option value="Piemonte">Piemonte</option>
+                <option value="Puglia">Puglia</option>
+                <option value="Sardegna">Sardegna</option>
+                <option value="Sicilia">Sicilia</option>
+                <option value="Toscana">Toscana</option>
+                <option value="Trentino-Alto Adige">Trentino-Alto Adige</option>
+                <option value="Umbria">Umbria</option>
+                <option value="Valle d'Aosta">Valle d'Aosta</option>
+                <option value="Veneto">Veneto</option>
+            </select>
+            <x-input-error :messages="$errors->get('region')" class="mt-2" />    
+        </div>
+
+        <!-- City -->
+        <div class="form-group mb-3">
+            <x-input-label for="city" :value="__('Città')" />
+            <input class="form-control mt-1" type="text" name="city" id="city" placeholder="Cognome" value="{{ old('city')}}" >
+            <x-input-error :messages="$errors->get('city')" class="mt-2" />    
+        </div>
+
 
         <!-- Password -->
         <div class="mt-4">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css','resources/sass/app.scss', 'resources/js/app.js'])
@@ -21,10 +24,10 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
-
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
+        
     </body>
 </html>
