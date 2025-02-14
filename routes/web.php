b6ca4b6811c543493991b3781159c7f5128588b9
nicomanunta <nic.manun@gmail.com>
2025-02-14 15:35:28 +0100

    complete project

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\Admin\EventDressCodeController;
 use App\Http\Controllers\Admin\EventLocationController;
 use App\Http\Controllers\Admin\FavoriteController;
-use App\Http\Controllers\Admin\GallaryController;
+use App\Http\Controllers\Admin\GalleryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 // ROTTE CRUD
 Route::middleware('auth', 'verified')->name('admin.')->group(function () {
-    Route::resource('galleries', GallaryController::class);
+    Route::resource('galleries', GalleryController::class);
     Route::resource('favorites', FavoriteController::class);
     Route::resource('event-locations', EventLocationController::class);
     Route::resource('event-dress-codes', EventDressCodeController::class);
